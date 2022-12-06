2e0041285d68508f4abe80448fd65ebbdb26660e
Md.Raihanul Haque(Shuvo) <67226278+Shuvoahmed29@users.noreply.github.com>
2022-12-06 11:16:36 +0600

    Add files via upload

--- 7.Binary Search.cpp
+++ 7.Binary Search.cpp
@@ -1,30 +1,41 @@
-// 7.Binary Search.
+// 7.Binary Search.cpp
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    cout << "Enter number of array element: ";
+    cout << "Enter array size: ";
     int n;
     cin >> n;
-    int array[n];
-    for (auto &d : array)
+    int arr[n];
+    cout << "Enter " << n << " size array!!\n";
+    for (int &d : arr)
         cin >> d;
-    sort(array + 0, array + n);
-    cout << "Which element you want to Search: ";
+    cout << "Which element you want to search: ";
     int element;
     cin >> element;
-    int first = 0, last = n - 1;
+
+    cout << "\nBefore sort!!\n";
+    for (int d : arr)
+        cout << d << " ";
+
+    sort(arr, arr + n);
+
+    int first = 0, last = n - 1, p = 0;
     while (first <= last)
     {
         int mid = (first + last) / 2;
-        if (element > array[mid])
-            first = mid + 1;
-        else if (element < array[mid])
-            last = mid - 1;
-        if (element == array[mid])
+        if (arr[mid] == element)
         {
-            cout << element << " is found in index: " << mid + 1 << endl;
+            cout << "\nElement is found in " << mid + 1 << " index!\n";
+            p = 1;
             break;
         }
+        else if (arr[mid] < element)
+            first = mid + 1;
+        else if (arr[mid] > element)
+            last = mid - 1;
     }
+
+    if (p == 0)
+        cout << "\nElement is not found!!\n";
 }
--- 8.Linear Search.cpp
+++ 8.Linear Search.cpp
@@ -10,14 +10,19 @@ int main()
     for (auto &d : array)
         cin >> d;
     cout << "Which element you want to search: ";
-    int element;
+    int element, p = 0;
     cin >> element;
+
     for (int i = 0; i < n; i++)
     {
         if (array[i] == element)
         {
             cout << element << " is found in index: " << i + 1 << endl;
+            p = 1;
             break;
         }
     }
+
+    if (p == 0)
+        cout << "Element is not found!!\n";
 }
